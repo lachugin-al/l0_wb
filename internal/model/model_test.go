9a8ca5648725870d6a2ff1d020e8fe47d0574e77
lachugin-al <anton.lachugin.l@gmail.com>
2024-12-15 00:16:24 +0300

    add model, model_test

---
+++ internal/model/model_test.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"testing"
+	"time"
+)
+
+// TestOrder_Validation проверяет корректность валидации объекта Order.
+//
+// 1. Убедиться, что заказ без items считается некорректным.
+// 2. Проверить, что добавление items в заказ исправляет проблему.
+func TestOrder_Validation(t *testing.T) {
+	order := &Order{
+		OrderUID:    "b563feb7b2b84b6test",
+		TrackNumber: "WBILMTESTTRACK",
+		Locale:      "en",
+		DateCreated: time.Now(),
+	}
+
+	// 1-я проверка
+	if len(order.Items) != 0 {
+		t.Errorf("expected items in the order = 0, got %d", len(order.Items))
+	}
+
+	// 2-я проверка
+	order.Items = []Item{{ChrtID: 123}}
+	if len(order.Items) == 0 {
+		t.Errorf("expected items in the order > 0, got %d", len(order.Items))
+	}
+}
