2632ef371f092f2f5e8ace7dd623e73fa037a83b
lachugin-al <anton.lachugin.l@gmail.com>
2024-12-16 16:43:01 +0300

    add model test

--- internal/model/model_test.go
+++ internal/model/model_test.go
@@ -7,9 +7,9 @@ import (
 
 // TestOrder_Validation проверяет корректность валидации объекта Order.
 //
-//	Проверки:
-//	1. Убедиться, что заказ без items считается некорректным.
-//	2. Проверить, что добавление items в заказ исправляет проблему.
+//	Сценарии:
+//	- Убедиться, что заказ без items считается некорректным.
+//	- Проверить, что добавление items в заказ исправляет проблему.
 func TestOrder_Validation(t *testing.T) {
 	order := &Order{
 		OrderUID:    "b563feb7b2b84b6test",
@@ -18,12 +18,12 @@ func TestOrder_Validation(t *testing.T) {
 		DateCreated: time.Now(),
 	}
 
-	// 1-я проверка
+	// 1-й сценарий
 	if len(order.Items) != 0 {
 		t.Errorf("expected items in the order = 0, got %d", len(order.Items))
 	}
 
-	// 2-я проверка
+	// 2-й сценарий
 	order.Items = []Item{{ChrtID: 123}}
 	if len(order.Items) == 0 {
 		t.Errorf("expected items in the order > 0, got %d", len(order.Items))
